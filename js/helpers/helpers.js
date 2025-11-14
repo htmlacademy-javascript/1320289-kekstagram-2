a56105c5b3f644cdded0b3e9ee7ddf68db9fed9b
Nick Horbach <creat.photo.art@gmail.com>
2025-11-14 17:05:00 +0200

    [M5-T1] Add modules

--- js/helpers/helpers.js
+++ js/helpers/helpers.js
@@ -39,24 +39,3 @@ export const getRandomNumberInRange = (min, max) =>
 
 export const getRandomArrayElement = (elements) =>
   elements[Math.floor(Math.random() * elements.length)];
-
-const fetchNamesFromPlaceholder = async (count) => {
-  try {
-    const response = await fetch(
-      'https://jsonplaceholder.typicode.com/comments',
-    );
-    if (!response.ok) {
-      throw new Error('Failed to fetch user data');
-    }
-    const data = await response.json();
-    const comments = data.slice(0, count).map((user) => user.name);
-    const emails = data.slice(0, count).map((user) => user.email);
-    return { comments, emails };
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Error fetching data:', error);
-    throw error;
-  }
-};
-
-export const initData = async (count) => await fetchNamesFromPlaceholder(count);
